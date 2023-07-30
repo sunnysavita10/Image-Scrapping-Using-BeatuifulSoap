ba750e366dd22e7502c598e32237f605d1ea0d18
SUNNY SAVITA <engsunny3365@gmail.com>
2023-07-30 12:19:28 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
+#from urllib.request import urlopen as uReq
 import logging
 import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
